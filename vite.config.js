1dbfdd72857bb28138706b73b45c7e6af8851904
Kamal0078k <saikamal0078@gmai.com>
2023-03-28 08:23:04 +0530

    Invoice

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+import { fileURLToPath } from "node:url";
 import { pdf } from "@progress/kendo-drawing";
 
 // https://vitejs.dev/config/
@@ -9,7 +11,11 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      external: [kendo - react - pdf],
+      external: [
+        "@progress/kendo-react-pdf",
+        fileURLToPath(new URL("src/Components/Preview.jsx", import.meta.url)),
+        /node_modules/,
+      ],
     },
   },
 });
